587aaaf984d6d3701652ab768ab3e97bc4f13518
adel ashraf <adel4ashraf@outlook.com>
2022-09-07 17:03:55 +0200

    modified admin auth login status code

--- app/Http/Controllers/AdminAuthController.php
+++ app/Http/Controllers/AdminAuthController.php
@@ -57,7 +57,7 @@ class AdminAuthController extends Controller
             'token' => $token,
         ];
 
-        return response($response, 201);
+        return response($response, 200);
     }
 
     public function logout()
