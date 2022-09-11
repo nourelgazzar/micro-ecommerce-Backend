b093e65c93067285d5445614985e17dbfd0d9a97
adel ashraf <adel4ashraf@outlook.com>
2022-09-11 13:39:00 +0200

    roles

--- app/Http/Controllers/AdminAuthController.php
+++ app/Http/Controllers/AdminAuthController.php
@@ -22,10 +22,7 @@ class AdminAuthController extends Controller
             'email' => $request->email,
             'password' => bcrypt($request->password),
         ]);
-<<<<<<< HEAD
-=======
-        
->>>>>>> efc6b7f44b5ba89c2fe4b446466592dbd8fd7e61$admin->assignRole('admin');
+        $admin->assignRole('admin');
         $response = [
             'admin' => $admin,
         ];
