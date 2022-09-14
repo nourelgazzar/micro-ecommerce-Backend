54107ce7ccdc0bb310845837cc44f29e69fcab8e
mahmoudrabea50 <111971891+mahmoudrabea50@users.noreply.github.com>
2022-09-14 15:32:35 +0200

    made the analytics

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1,13 +1,65 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\Brand;
+use App\Models\OrderDetail;
 
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    public function analytices()
+    public function analytics()
     {
+        $total_prices = 0;
+        $orders = Order::all();
+        foreach($orders as $order){
+          $total_prices += $order->price;
+        }
+
+        $products = Product::pluck('id');
+        $max_items = 0;
+        $max_items_id = 0;
+        $brands_number_of_sold_items = array_fill(0, 100, 0);
+
+        foreach ($products as $product)
+        {
+            $add_items = 0;
+            $products_orders = OrderDetail::where('product_id', '=', $product)->get();
+            foreach($products_orders as $product_order)
+            {
+                $add_items += $product_order->number_items;
+                $id = Product::find($product_order->product_id)->value('brand_id');
+                $brands_number_of_sold_items[$id] += $product_order->number_items;
+            }
+            if ($add_items > $max_items)
+            {
+                $max_items = $add_items;
+                $max_items_id = $product;
+            }
+        }
+
+        $max_items_of_brands = 0;
+        $max_items_of_brands_id = 0;
+        for($i = 1; $i < 100; $i++)
+        {
+            if ($brands_number_of_sold_items[$i] > $max_items_of_brands)
+            {
+                $max_items_of_brands = $brands_number_of_sold_items[$i];
+                $max_items_of_brands_id = $i;
+            }
+        }
         
+
+        return response()->json([
+            'status' => 200,
+            'total_prices' => $total_prices,
+            'most sold product' => Product::find($max_items_id),
+            'number of items of most sold product' => $max_items,
+            'most sold brand' => Brand::find($max_items_of_brands_id),
+            'number of items of most sold brand' => $max_items_of_brands,
+
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\BrandController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\CartController;
 use Illuminate\Support\Facades\Route;
 
@@ -42,6 +43,7 @@ Route::group(['prefix' => 'admin/', 'middleware' => ['auth:sanctum', 'role:admin
     Route::get('categories/{id}', [CategoryController::class, 'show']);
     Route::put('categories/{id}', [CategoryController::class, 'update']);
     Route::delete('categories/{id}', [CategoryController::class, 'destroy']);
+    Route::get('analytics', [AdminController::class, 'analytics']);
 
 
     Route::group(
